94369c9a833b6e6ec60b270c8affc353007f6eca
end3rs-army <eric.m.dowty@gmail.com>
2018-08-26 19:38:44 -0600

    Uncomment test code

--- spec/fizz_buzz_spec.js
+++ spec/fizz_buzz_spec.js
@@ -1,19 +1,19 @@
 var FizzBuzz = require("../fizz_buzz.js").FizzBuzz;
 
 describe("FizzBuzz", function() {
-  // it("should return Fizz for n=1", function() {
-  //   expect(FizzBuzz(1)).toEqual("Fizz");
-  // });
-  //
-  // it("should return Fizz Fizz for n=2", function() {
-  //   expect(FizzBuzz(2)).toEqual("Fizz Fizz");
-  // });
-  //
-  // it("should return Fizz Fizz Buzz for n=3", function() {
-  //   expect(FizzBuzz(3)).toEqual("Fizz Fizz Buzz");
-  // });
-  //
-  // it("should return correct for large number", function() {
-  //   expect(FizzBuzz(9)).toEqual("Fizz Fizz Buzz Fizz Fizz Buzz Fizz Fizz Buzz");
-  // });
+  it("should return Fizz for n=1", function() {
+    expect(FizzBuzz(1)).toEqual("Fizz");
+  });
+
+  it("should return Fizz Fizz for n=2", function() {
+    expect(FizzBuzz(2)).toEqual("Fizz Fizz");
+  });
+
+  it("should return Fizz Fizz Buzz for n=3", function() {
+    expect(FizzBuzz(3)).toEqual("Fizz Fizz Buzz");
+  });
+
+  it("should return correct for large number", function() {
+    expect(FizzBuzz(9)).toEqual("Fizz Fizz Buzz Fizz Fizz Buzz Fizz Fizz Buzz");
+  });
 });
